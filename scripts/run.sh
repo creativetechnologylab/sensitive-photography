b1c5f71761e3e28b3b44352dfc01af550b92ea45
garethfoote <gareth.foote@gmail.com>
2016-07-07 16:07:19 +0100

    Changing location

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,5 +11,13 @@ fi
 cd $DIR/../node-red && npm install
 cd $DIR/../node-red && ./run.sh
 
+# Images directory.
+mkdir ~/Desktop/audio/
+
+# Change config.
+sed -i 's/gfoote/$USER/g' $DIR/../node-red/flow.json
+sed -i 's/gfoote/$USER/g' $DIR/../node-red/flow_cred.json
+
 open ${DIR}/../processing/ui/application.macosx/ui.app
 open http://127.0.0.1:1880/
+
